455e3a72afd24d5838051833956d3ae17d43508c
shakeer <mdshakeer91@gmail.com>
2019-06-09 23:04:59 +0530

    file reader

---
+++ file_reader.rb
@@ -0,0 +1,10 @@
+def file_to_array
+	a = []
+	#Open text file in read mode 
+	File.open("dictionary.txt", "r+") do |f|
+		f.each do |line|
+			a << line
+		end
+	end
+	return a
+end
---
+++ text_generate.rb
@@ -0,0 +1,8 @@
+require_relative 'file_reader'
+
+start_time = Time.now
+puts file_to_array.size
+end_time = Time.now - start_time
+puts end_time
+
+
