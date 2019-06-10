3ebd91ec315017b07375b0a96576f52160c89093
shakeer <mdshakeer91@gmail.com>
2019-06-10 23:46:07 +0530

    combination of search logic stage1

--- file_reader.rb
+++ file_reader.rb
@@ -1,10 +1,22 @@
 def file_to_array
-	a = []
+	a3,a4,a5,a6,a7,a10 = [],[],[],[],[],[]
 	#Open text file in read mode 
 	File.open("dictionary.txt", "r+") do |f|
 		f.each do |line|
-			a << line.chomp
+			if line.length == 3
+				a3 << line.chomp.downcase
+			elsif line.length == 4
+				a4 << line.chomp.downcase
+			elsif line.length == 5
+				a5 << line.chomp.downcase
+			elsif line.length == 6
+				a6 << line.chomp.downcase
+			elsif line.length == 7
+				a7 << line.chomp.downcase
+			elsif line.length == 10
+				a10 << line.chomp.downcase
+			end
 		end
 	end
-	return a
+	return [a3,a4,a5,a6,a7,a10]
 end
--- text_generate.rb
+++ text_generate.rb
@@ -5,7 +5,8 @@ def generate_text_for(num)
 	mapped_text = {"2" => ["a","b","c"],"3" => ["d","e","f"], "4" => ["g","h","i"], "5" => ["j","k","l"], "6"=>["m","n","o"], "7" => ["p","q","r","s"], "8" => ["t","u","v"], "9" => ["w","x","y","z"]}
 	text_list = file_to_array
 	com_list = []
-	list = []
+	filtered_list = []
+	list3a,list3b,list3c,list3d,list4a,list4b,list5a,list5b,list6a,list6b,list7a,list7b,list10 = [],[],[],[],[],[],[],[],[],[],[],[],[]
 	if num.length == 10
 		num.split("").each do |le|
 			com_list << mapped_text[le]
@@ -20,7 +21,19 @@ def generate_text_for(num)
 									com_list[7].each do |h|
 										com_list[8].each do |i|
 											com_list[9].each do |j|
-												list << (a+b+c+d+e+f+g+h+i+j).upcase.chomp
+												list3a << (a+b+c).chomp
+												list3b << (d+e+f).chomp
+												list3c << (h+i+j).chomp
+												list3d << (e+f+g).chomp
+												list4a << (a+b+c+d).chomp
+												list4b << (g+h+i+j).chomp
+												list5a << (a+b+c+d+e).chomp
+												list5b << (f+g+h+i+j).chomp
+												list6a << (e+f+g+h+i+j).chomp
+												list6b << (a+b+c+d+e+f).chomp
+												list7a << (a+b+c+d+e+f+g).chomp
+												list7b << (d+e+f+g+h+i+j).chomp
+												list10 << (a+b+c+d+e+f+g+h+i+j).chomp
 											end
 										end
 									end
@@ -33,9 +46,9 @@ def generate_text_for(num)
 		end
 		# puts text_list[98081]
 		# puts list[26638]
-		filtered_list = text_list & list
+		filtered_list << text_list[5] & list10
 		if filtered_list.size > 0
-			puts "1-800-"+filtered_list[0]
+			puts "1-800-#{filtered_list[0]}"
 		end
 	else
 		puts "length should be equal to 10"
